a9b7848c14167d91dd7b1916559dd3998711ca5c
Sumin Byeon <suminb@gmail.com>
2017-04-03 17:42:32 +0900

    Parse values from CSV to generate AssetValue

--- goport/src/finance/importer.go
+++ goport/src/finance/importer.go
@@ -4,8 +4,12 @@ import (
 	"bufio"
 	"encoding/csv"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func ReadStockValues(filePath string) {
@@ -15,10 +19,32 @@ func ReadStockValues(filePath string) {
 	}
 
 	r := csv.NewReader(bufio.NewReader(f))
-	records, err := r.ReadAll()
-	if err != nil {
-		log.Fatal(err)
-	}
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	fmt.Print(records)
+		// FIXME: Could we make the following code a bit cleaner?
+		evaluatedAt, _ := time.Parse("2006-1-2", record[0])
+		open, _ := strconv.ParseFloat(strings.TrimSpace(record[1]), 64)
+		high, _ := strconv.ParseFloat(strings.TrimSpace(record[2]), 64)
+		low, _ := strconv.ParseFloat(strings.TrimSpace(record[3]), 64)
+		close, _ := strconv.ParseFloat(strings.TrimSpace(record[4]), 64)
+		volume, _ := strconv.ParseInt(strings.TrimSpace(record[5]), 10, 64)
+
+		av := AssetValue{
+			EvaluatedAt: evaluatedAt,
+			Open:        open,
+			High:        high,
+			Low:         low,
+			Close:       close,
+			Volume:      volume,
+		}
+
+		fmt.Println(av)
+	}
 }
