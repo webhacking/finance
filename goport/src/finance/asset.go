3c1c1d7ae3e51f96216fcaa1dff7412d0c2c53f7
Sumin Byeon <suminb@gmail.com>
2019-04-09 01:50:00 +0900

    Read .tsv files

--- goport/src/finance/asset.go
+++ goport/src/finance/asset.go
@@ -1,5 +1,13 @@
 package finance
 
+import (
+	"bufio"
+	"encoding/csv"
+	"io"
+	"log"
+	"os"
+)
+
 // Asset represents investment vehicles such as stocks, bonds, funds, deposits, options, futures, and such.
 type Asset struct {
 	Name      string
@@ -20,7 +28,33 @@ const (
 	Deposit AssetType = 5
 )
 
-// LoadAssetsFromTsv loads assets from a .tsv (tab separated values) file
-func LoadAssetsFromTsv(filename string) {
+// GetReaderFromFile returns a reader object from a file
+func GetReaderFromFile(filename string) io.Reader {
+	file, _ := os.Open(filename)
+	return bufio.NewReader(file)
+}
 
+// LoadAssetsFromTsv loads assets from a .tsv (tab separated values) file
+func LoadAssetsFromTsv(filename string) []Asset {
+	reader := csv.NewReader(GetReaderFromFile(filename))
+	reader.Comma = rune('\t')
+
+	var assets []Asset
+	for {
+		cols, error := reader.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatalln(error)
+		}
+
+		asset := Asset{
+			Name: cols[0],
+			Code: cols[1],
+			Type: Fund, // TODO: Parse string to make an enum
+		}
+		assets = append(assets, asset)
+	}
+
+	return assets
 }
