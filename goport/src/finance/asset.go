06cf4671861f10642bad82bfe956b3913aef5f9c
Sumin Byeon <suminb@gmail.com>
2019-04-09 02:34:30 +0900

    Fetch fund values

--- goport/src/finance/asset.go
+++ goport/src/finance/asset.go
@@ -3,9 +3,16 @@ package finance
 import (
 	"bufio"
 	"encoding/csv"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/text/encoding/korean"
 )
 
 // Asset represents investment vehicles such as stocks, bonds, funds, deposits, options, futures, and such.
@@ -57,4 +64,40 @@ func LoadAssetsFromTsv(filename string) []Asset {
 	}
 
 	return assets
+}
+
+// e.g., http://www.fundnuri.com/fr/fr_ovw.asp?FUND_CD=K55232BU5747
+// TODO: Move this function elsewhere
+func FetchFundInfo(code string) (float64, error) {
+	url := fmt.Sprintf("http://www.fundnuri.com/fr/fr_ovw.asp?FUND_CD=%s", code)
+	headers := make(map[string]string)
+	params := make(map[string]string)
+
+	resp, error := Fetch(url, headers, params)
+	if error != nil {
+		log.Fatal(error)
+	}
+	defer resp.Body.Close()
+
+	reader := ForceConvertEncoding(resp.Body, korean.EUCKR)
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// FIXME: Refactoring needed
+	var value string
+	r, _ := regexp.Compile("[0-9,.]+")
+	elements := doc.Find("td span")
+	elements.Each(func(i int, s *goquery.Selection) {
+		text := strings.Trim(s.Text(), " ")
+		if text == "기준가" {
+			node := s.Parent().Parent().Next().Children()
+			value = r.FindString(node.Text())
+		}
+	})
+
+	return strconv.ParseFloat(value, 64)
 }
--- goport/src/finance/asset_test.go
+++ goport/src/finance/asset_test.go
@@ -9,3 +9,12 @@ func TestLoadAssetsFromTsv(t *testing.T) {
 	assets := LoadAssetsFromTsv("assets.tsv")
 	fmt.Println(assets)
 }
+
+func TestFetchFundInfo(t *testing.T) {
+	value, _ := FetchFundInfo("K55216BY4761")
+	fmt.Println(value)
+	// expected := 881.62
+	// if value != expected {
+	// 	assertEquals(t, expected, value, "Incorrect value")
+	// }
+}
