7bcd103d387a1d4f7e3b18a4c13a7ec376a4bb48
Sumin Byeon <suminb@gmail.com>
2019-04-09 00:38:22 +0900

    Getting started with Golang

---
+++ goport/src/finance/finance.go
@@ -0,0 +1,47 @@
+package finance
+
+type Portfolio struct {
+	Records []PortfolioRecord
+}
+
+type PortfolioRecord struct {
+	Asset        Asset
+	Quantity     float64
+	DesiredShare float64
+}
+
+// RebalancePlan indicates how much of the asset must be purchased or sold
+type RebalancePlan struct {
+	Asset    Asset
+	Quantity float64
+}
+
+// NOTE: Not sure if taking *Portfolio as a parameter, but couldn't think of a better way
+func (record *PortfolioRecord) CurrentShare(portfolio *Portfolio) float64 {
+	nav := portfolio.CalcNetAssetValue()
+	return record.Asset.UnitPrice * record.Quantity / nav
+}
+
+func (portfolio *Portfolio) CalcNetAssetValue() float64 {
+	netAssetValue := 0.0
+	for _, record := range portfolio.Records {
+		netAssetValue += record.Asset.UnitPrice * record.Quantity
+	}
+	return netAssetValue
+}
+
+// Rebalance a given portfolio according to the desired shares of each asset
+// TODO: Need an option to determine whether we want to allow non-discrete quantities
+func (portfolio *Portfolio) Rebalance() []RebalancePlan {
+	plans := make([]RebalancePlan, len(portfolio.Records))
+	nav := portfolio.CalcNetAssetValue()
+	for i, record := range portfolio.Records {
+		unitPrice := record.Asset.UnitPrice
+		currentValue := unitPrice * record.Quantity
+		targetValue := record.DesiredShare * nav
+
+		plans[i].Asset = record.Asset
+		plans[i].Quantity = (targetValue - currentValue) / unitPrice
+	}
+	return plans
+}
